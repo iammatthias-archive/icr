b0c8c1f8b30ccafa9560d84ee6a711e618cb7211
Matthias Jordan <matthias.k.jordan@gmail.com>
2020-07-06 21:30:25 +0000

    maple leaf

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/turtle.png`,
+                icon: `src/images/maple-leaf.png`,
             },
         },
         `gatsby-plugin-styled-components`,

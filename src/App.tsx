0e226ee2930a0cc88500182f3e3eec767cedec7c
Matthias Jordan <matthias.k.jordan@gmail.com>
2025-03-04 04:58:55 +0000

    reinit - migrate from gatsby to vite + react, change from joke site to factual site.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section>
+        <img src="https://media.giphy.com/media/MONvE9pT7jsEE/giphy.gif" />
+      </section>
+      <section>
+        <h1>yes.</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
